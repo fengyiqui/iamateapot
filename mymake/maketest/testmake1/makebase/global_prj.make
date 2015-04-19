7d70e6d137342ce3839b0e7aaccf608e21271c51
unknown <fengyiqui@163.com>
2015-04-19 13:21:44 +0800

    添加readme 修改surecp.sh

--- mymake/maketest/testmake1/makebase/global_prj.make
+++ mymake/maketest/testmake1/makebase/global_prj.make
@@ -1,6 +1,7 @@
 #对prj的全局配置，每个prj都用的
 #会被prj.make覆盖
-SolutionDir := /mnt/shared/test/cpp/maketest/testmake1
+#SolutionDir := /mnt/shared/test/cpp/maketest/testmake1
+SolutionDir := /mnt/share/workshop/iamateapot/mymake/maketest/testmake1
 #全局使用的头文件包含路径
 G_AdditionalIncludeDir :=
 #全局使用的依赖库
--- mymake/maketest/testmake1/makebase/surecp.sh
+++ mymake/maketest/testmake1/makebase/surecp.sh
@@ -1,13 +1,9 @@
-#!/bin/sh
-# 判断目标是否是文件，获得目标目录
-# 下面替换的意思是把从行尾向左的第一个'/'号之间的东西全部替换为空
-#DSTDIR=`echo $2 | sed -e 's/\/[^\/]*$//'`
-DSTDIR=$2
-# 判断目标目录是否存在，不存在就创建之
-if [ ! -d $DSTDIR ]
-then
-	mkdir -p $DSTDIR
+#!/bin/bash
+DstDir=$2
+if [ ! -d "$DstDir" ]; then
+		mkdir -p "$DstDir"
 fi
-# 拷贝文件
-echo surecopying $1 to $2 ...
+
+echo "surecopying $1 to $2"
+
 cp -f $1 $2
